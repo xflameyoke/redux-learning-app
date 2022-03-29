65cc2d85838ccac94a9acaedfe01fd073732a4aa
Mateusz Łuczak <luczakmmateusz@gmail.com>
2022-03-29 23:44:38 +0200

    toogle counter

--- src/components/counter/counter.tsx
+++ src/components/counter/counter.tsx
@@ -3,14 +3,17 @@ import { useSelector, useDispatch } from 'react-redux'
 import { CounterStyled, ValueStyled } from './counter.styled'
 
 interface RootState {
+    showCounter: boolean
     counter: number
 }
 
 const numberChange = (state: RootState) => state.counter
+const showChange = (state: RootState) => state.showCounter
 
 const Counter = () => {
     const dispatch = useDispatch()
     const counter = useSelector(numberChange)
+    const show = useSelector(showChange)
 
     const incrementHandler = () => {
         dispatch({ type: 'increment' })
@@ -32,16 +35,26 @@ const Counter = () => {
         dispatch({ type: 'clear' })
     }
 
+    const increaseHandler = () => {
+        dispatch({ type: 'increase', value: 5 })
+    }
+
+    const toggleHandler = () => {
+        dispatch({ type: 'toggle' })
+    }
+
     return (
         <CounterStyled>
             <h1>Redux Counter</h1>
-            <ValueStyled>{counter}</ValueStyled>
+            {show && <ValueStyled>{counter}</ValueStyled>}
             <div>
                 <button onClick={incrementHandler}>Increment</button>
                 <button onClick={decrementHandler}>Decrement</button>
-                <button onClick={multiplyHandler}>Multiply</button>
-                <button onClick={divideHandler}>Divide</button>
+                <button onClick={multiplyHandler}>Multiply by 2</button>
+                <button onClick={divideHandler}>Divide by 2</button>
                 <button onClick={clearHandler}>Clear</button>
+                <button onClick={increaseHandler}>Increase by 5</button>
+                <button onClick={toggleHandler}>Toggle Counter</button>
             </div>
         </CounterStyled>
     )
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,33 +1,55 @@
 import { createStore } from 'redux'
 
-const counterReducer = (state = { counter: 0 }, action: { type: string }) => {
+const counterReducer = (
+    state = { counter: 0, showCounter: true },
+    action: { type: string; value: number }
+) => {
     if (action.type === 'increment') {
         return {
             counter: state.counter + 1,
+            showCounter: state.showCounter,
         }
     }
 
     if (action.type === 'decrement') {
         return {
             counter: state.counter - 1,
+            showCounter: state.showCounter,
         }
     }
 
     if (action.type === 'multiply') {
         return {
             counter: state.counter * 2,
+            showCounter: state.showCounter,
         }
     }
 
     if (action.type === 'divide') {
         return {
             counter: state.counter / 2,
+            showCounter: state.showCounter,
         }
     }
 
     if (action.type === 'clear') {
         return {
             counter: state.counter - state.counter,
+            showCounter: state.showCounter,
+        }
+    }
+
+    if (action.type === 'increase') {
+        return {
+            counter: state.counter + action.value,
+            showCounter: state.showCounter,
+        }
+    }
+
+    if (action.type === 'toggle') {
+        return {
+            showCounter: !state.showCounter,
+            counter: state.counter,
         }
     }
 
